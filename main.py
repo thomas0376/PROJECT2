c6796e1ea07cac83fae21c829dec8f0dcb27c70c
thomas0376 <thomas.prevarin@gmail.com>
2024-09-24 17:02:09 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# these are built in modules
+import platform
+import sys
+
+print( f"\nHello, I'm Python Version: {platform.python_version()}" )
+print( f"\nThis is where I live: {sys.executable}" )
+
+
